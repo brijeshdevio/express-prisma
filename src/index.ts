ab29a2af7a5c170df8387cb11b51bb7ee763500b
Brijesh <softbrijeshio@gmail.com>
2026-03-04 13:31:15 +0530

    refactor: update export syntax for app object

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express from "express";
 
-export const app = express();
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello from Express + TypeScript!");
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+export default app;
