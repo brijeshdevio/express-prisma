0155022d16db05423fa1bfba8308c30b469db7a0
Brijesh <softbrijeshio@gmail.com>
2026-03-04 13:43:57 +0530

    feat: add health endpoint to app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 export const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello from Express + TypeScript!");
+});
+
+app.get("/health", (req: Request, res: Response) => {
+  res.send("OK");
 });
