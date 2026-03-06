6ec0dd0cf3815ff8683f430a7400f368595c073c
Brijesh <softbrijeshio@gmail.com>
2026-03-06 11:23:12 +0530

    feat: create simple express app with health endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import type { Request, Response, Application } from "express";
 const app: Application = express();
 app.get("/", (req: Request, res: Response) => res.send("Hello World!"));
+app.get("/health", (req: Request, res: Response) => res.send("OK!"));
 
 if (process.env.NODE_ENV === "production") {
   app.listen(3000, () => console.log("Example app listening on port 3000!"));
