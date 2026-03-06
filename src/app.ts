3235014fc4a838a0552d66279643aaad7593d05e
Brijesh <softbrijeshio@gmail.com>
2026-03-06 10:13:26 +0530

    refactor: update import statements for express and request/response	types

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import type { Request, Response } from "express";
 import { prisma } from "./db";
 import { Prisma } from "./generated/prisma/client";
 
